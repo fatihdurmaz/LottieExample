664596779ab86a646318807f460ac4976b2537ba
Fatih Durmaz <71706970+fatihdurmaz@users.noreply.github.com>
2023-11-24 17:12:37 +0300

    Initial Commit

---
+++ LottieExample/LottieExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieExampleApp.swift
+//  LottieExample
+//
+//  Created by Fatih Durmaz on 24.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct LottieExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
