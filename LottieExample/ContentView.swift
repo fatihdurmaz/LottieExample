30f401a30a9e7f98ddd85ac01060fcba7e4ee018
Fatih Durmaz <71706970+fatihdurmaz@users.noreply.github.com>
2023-11-28 15:25:09 +0300

    first commit

--- LottieExample/ContentView.swift
+++ LottieExample/ContentView.swift
@@ -6,16 +6,45 @@
 //
 
 import SwiftUI
+import Lottie
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                
+                // JSON Dosyasından Oynatma
+                VStack {
+                    Text("JSON Dosyasından")
+                        .bold()
+                    LottieView(animation: .named("world"))
+                        .looping()
+                    .frame(width: 200, height: 200)
+                }
+                
+                Spacer()
+                
+                // URL Adresinden Oynatma
+                VStack {
+                    Text("URL Adresinden")
+                        .bold()
+                    LottieView {
+                        await LottieAnimation.loadedFrom(url: URL(string: "https://lottie.host/8d80d123-e13f-484f-8ce0-0fe220e0888f/v9Ar0kgFP3.json")!)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .looping()
+                .frame(width: 200, height: 200)
+                }
+                
+                
+            }
+            .padding()
+            .navigationTitle("Lottie Example")
+            .toolbarBackground(.visible, for: .navigationBar)
+            .toolbarBackground(.teal)
         }
-        .padding()
     }
 }
 
